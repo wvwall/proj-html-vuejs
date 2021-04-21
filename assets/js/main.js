f4366596026241814693e24ab41c20d2df20e650
wvwall <waltervelardo@gmail.com>
2021-04-21 17:30:25 +0200

    Jumbo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,28 @@ Vue.config.devtools = true;
 var app = new Vue ({
     el: '#root',
     data: {
-     
+     listNav : [
+
+        {
+            name : "home",
+        },
+
+        {
+            name : "services",
+        },
+
+        {
+            name : "about",
+        },
+
+        {
+            name : "projects",
+        },
+
+        {
+            name : "results",
+        }
+     ]
     },
     
   });
