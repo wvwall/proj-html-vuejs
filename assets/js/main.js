3d88a0197c665d6505e0ba9b9d68e689348abe16
wvwall <waltervelardo@gmail.com>
2021-04-22 15:44:45 +0200

    Section Cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,38 @@ var app = new Vue ({
         {
             name : "results",
         }
+     ],
+     img : [
+
+        {
+            name : "./assets/img/project-1.jpg",
+            text : "Acadedmia professional program in social media"
+        },
+
+        {
+            name : "./assets/img/project-2.jpg",
+            text : "President's speech at the annual meeting"
+        },
+
+        {
+            name : "./assets/img/project-3.jpg",
+            text : "Intenational business trip in Shanghai"
+        },
+
+        {
+            name : "./assets/img/project-4.jpg",
+            text : "Technology workshop with education theme"
+        },
+
+        {
+            name : "./assets/img/project-5.jpg",
+            text : "Donation of clothes and food to the partner NGO"
+        },
+
+        {
+            name : "./assets/img/project-6.jpg",
+            text : "Confraternization of the procurement team"
+        },
      ]
     },
     
